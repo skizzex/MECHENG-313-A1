b341b7a50d9dcc95bf4df01407a74825cf406a73
knyu <knyu88@gmail.com>
2023-04-17 18:44:52 +1200

    added code for turning LED on

--- Task2.1/Task2.1.ino
+++ Task2.1/Task2.1.ino
@@ -1,23 +1,35 @@
 #include <avr/interrupt.h>
 
+//define global variables
+
+//timer ISR to update every second
+
+//ADC ISR to record ADC value
+
+
+//blink the LED a number of times
+//input: number of blinks
+void blink(uint8_t blinks) {
+
+}
+
 int main(void) {
+  sei();  //enable global interrupts
 
-  //enable global interrupts
+  //set pin functions(LED, ADC) (N/A?)
 
-  //set pin functions(LED, ADC)
+  DDRB |= (1 << DDB5); //set built-in LED pin to output
 
-  //set inputs/outputs (on board LED)
+  //testing - turn LED on
+  PORTB |= (1 << PORTB5);
 
   //configure ADC
 
-  //configure timer 1 (16 bit timer at 1MHz) to CTC mode to 
+  //configure timer 1 (16 bit timer at 1MHz) to CTC mode to read ADC every second
 
 
 
   while (1) {
 
   }
-
-
-
 }
