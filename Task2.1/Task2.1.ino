c06910def031e055e058fc0a09edbea6ff6fd1fe
knyu <knyu88@gmail.com>
2023-04-18 11:06:25 +1200

    added timer for 1 second period and variable input period for blinking

--- Task2.1/Task2.1.ino
+++ Task2.1/Task2.1.ino
@@ -27,7 +27,9 @@ ISR(TIMER1_OVF_vect) {
 }
 
 //ADC ISR to record ADC value
-
+ISR(ADC_vect) {
+  
+}
 
 //initialise built-in LED
 void LED_init() {
@@ -35,23 +37,25 @@ void LED_init() {
 }
 
 //initialise timer 0 for blinking the LED
-void timer0_init() {
+//input: period for toggling LED on/off
+//(given prescaler of 1024, max period of 0.262 secs allowed and min period of 1ms)
+void timer0_init(float togglePeriod) {
   TCCR0B &= ~(CS02 | CS01 | CS00); //stop timer by setting no clock source
 
   TCCR0A |= (1 << WGM01); //set timer 0 to CTC mode
   TCCR0B |= (1 << CS02) | (1 << CS00); //start timer and set timer 0 clock prescaler to 1024
 
-  OCR0A = 194;  //set compare register A value to 194 for LED toggle period of about 0.2s (see notes for calculation)
+  OCR0A = (uint8_t)(1000000*togglePeriod/1024 - 1);  //set compare register A value to 194 for LED toggle period (clock is at 16MHz so ISR is entered at every 16th period)
 }
 
 //initialise timer 1 for ADC sampling every second
 void timer1_init() {
   TCCR1B &= ~(CS12 | CS11 | CS10); //stop timer by setting no clock source
 
-  TCCR1A = 0; //TESTING - timer 1 in normal mode
-  TCCR1B |= (1 << CS10); //TESTING - no prescaler
+  TCCR1B |= (1 << WGM12); //set timer 1 to CTC mode
+  TCCR1B |= (1 << CS12); //start timer and set timer 0 clock prescaler to 256
 
-  TIMSK1 |= (1 << TOIE1); //TESTING - overflow interrupt enable
+  OCR1A = 62500; //set compare register A value to 62500 for period of 1 sec
 }
 
 //initialise ADC
@@ -72,18 +76,22 @@ void blinkLED(uint8_t blinks) {
   TIMSK0 |= (1 << OCIE0A);  //enable comp A interrupt 
 }
 
+//test ADC with LED for task 2.1
+void startADC() {
+
+}
+
 int main(void) {
   sei();  //enable global interrupts
 
   LED_init();
-  timer0_init();  //configure timer 0 to toggle LED every 0.2 seconds (CLOCK IS AT 16MHz)
-  // timer1_init();  //configure timer 1 (16 bit timer at 16MHz) to CTC mode to read ADC every second
+  timer0_init(0.2);  //configure timer 0 to toggle LED every 0.2 seconds (CLOCK IS AT 16MHz)
+  timer1_init();  //configure timer 1 (16 bit timer at 16MHz) to CTC mode to read ADC every second
     //configure ADC
 
-  blinkLED(2);
+  blinkLED(5);
 
   while (1) {
-    //begin ADC 
-
+    //check ADC value every second and blink depending on if lower or higher than compare value
   }
 }
