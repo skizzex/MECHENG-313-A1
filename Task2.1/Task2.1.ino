9427b5ae1699c079f8b12a0ec355396705ffc37c
knyu <knyu88@gmail.com>
2023-04-17 14:29:34 +1200

    initial commit

---
+++ Task2.1/Task2.1.ino
@@ -0,0 +1,23 @@
+#include <avr/interrupt.h>
+
+int main(void) {
+
+  //enable global interrupts
+
+  //set pin functions(LED, ADC)
+
+  //set inputs/outputs (on board LED)
+
+  //configure ADC
+
+  //configure timer 1 (16 bit timer at 1MHz) to CTC mode to 
+
+
+
+  while (1) {
+
+  }
+
+
+
+}
